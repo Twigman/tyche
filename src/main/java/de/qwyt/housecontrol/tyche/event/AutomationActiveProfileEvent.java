66fadc7a387b5591037f47293b041b585368ba5f
Twigman <twigman@posteo.de>
2025-03-20 12:31:51 +0100

    added profile management via api

---
+++ src/main/java/de/qwyt/housecontrol/tyche/event/AutomationActiveProfileEvent.java
@@ -0,0 +1,19 @@
+package de.qwyt.housecontrol.tyche.event;
+
+import de.qwyt.housecontrol.tyche.model.profile.automation.AutomationProfileType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AutomationActiveProfileEvent extends TycheEvent {
+
+	private static final long serialVersionUID = 8874965415174095105L;
+	
+	private AutomationProfileType activeProfile;
+
+	public AutomationActiveProfileEvent(Object source, HousecontrolModule module, AutomationProfileType activeProfile) {
+		super(source, module);
+		this.activeProfile = activeProfile;
+	}
+}
--- src/main/java/de/qwyt/housecontrol/tyche/service/AutomationProfileManager.java
+++ src/main/java/de/qwyt/housecontrol/tyche/service/AutomationProfileManager.java
@@ -3,8 +3,11 @@ package de.qwyt.housecontrol.tyche.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import de.qwyt.housecontrol.tyche.event.HousecontrolModule;
+import de.qwyt.housecontrol.tyche.event.LogLevel;
 import de.qwyt.housecontrol.tyche.model.profile.automation.ActiveAutomationProfile;
 import de.qwyt.housecontrol.tyche.model.profile.automation.AutomationProfile;
 import de.qwyt.housecontrol.tyche.model.profile.automation.AutomationProfileProperties;
@@ -27,15 +30,20 @@ public class AutomationProfileManager {
 	
 	private AutomationProfile activeProfile;
 	
+	private final EventServiceImpl eventService;
+
+	
 	@Autowired
 	public AutomationProfileManager(
 			AutomationProfileProperties properties,
 			AutomationProfileRepository automationProfileRepository,
-			ActiveAutomationProfileRepository activeAutomationProfileRepository
+			ActiveAutomationProfileRepository activeAutomationProfileRepository,
+			EventServiceImpl eventService
 			) {
 		this.properties = properties;
 		this.automationProfileRepository = automationProfileRepository;
 		this.activeAutomationProfileRepository = activeAutomationProfileRepository;
+		this.eventService = eventService;
 	}
 	
 	@PostConstruct
@@ -55,12 +63,17 @@ public class AutomationProfileManager {
 		return properties.getProfiles().get(profileType);
 	}
 	
-	public boolean setActiveProfile(AutomationProfileType profileType) {
+	public boolean setActiveProfile(HousecontrolModule module, AutomationProfileType profileType) {
 		if (this.activeProfile.getProfileType() != profileType) {
 			// change profile
 			this.activeProfile = this.properties.getProfiles().get(profileType);
 			this.activeAutomationProfileRepository.save(new ActiveAutomationProfile(this.activeProfile.getProfileType()));
 			LOG.info("AUTOMATION PROFILE {} {}", Symbole.ARROW_RIGHT, this.activeProfile.getProfileType());
+			eventService.fireAutomationActiveProfile(module, activeProfile.getProfileType());
+			
+			if (this.activeProfile.isAutoHomeProfile()) {
+				LOG.info("Auto HOME is enabled");
+			}
 			
 			return true;
 		}
